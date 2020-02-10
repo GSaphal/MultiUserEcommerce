31e8c19cd9e7006f50298208ae44e84799bf1d4b
Saphal <gsaphal99@gmail.com>
2020-02-10 21:01:04 +0545

    Created Product -a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,19 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+   <h2>Products</h2>
+   <div class="row">
+   <div class="card">
+       <img class="card-img-top" src="holder.js/100x180/?text=Image cap" alt="Card image cap">
+       <div class="card-body">
+           <h4 class="card-title">Title</h4>
+           <p class="card-text">Text</p>
+       </div>
+       <div class="card-body">
+           <a href="#" class="card-link">Card link</a>
+           <a href="#" class="card-link">Another link</a>
+       </div>
+   </div>
+   </div>
 </div>
 @endsection
