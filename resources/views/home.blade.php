cccb9e551f38ab8eb05e85692387665942bf0fc5
Saphal <gsaphal99@gmail.com>
2020-02-10 21:09:31 +0545

    Created Display Product Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,21 @@
 <div class="container">
    <h2>Products</h2>
    <div class="row">
-   <div class="card">
-       <img class="card-img-top" src="holder.js/100x180/?text=Image cap" alt="Card image cap">
+@foreach ($allProducts as $products)
+
+   <div class="card col-4">
+       <img class="card-img-top" src="{{asset('default.png')}}" alt="Card image cap">
        <div class="card-body">
-           <h4 class="card-title">Title</h4>
+           <h4 class="card-title">{{$products->name}}</h4>
            <p class="card-text">Text</p>
        </div>
        <div class="card-body">
-           <a href="#" class="card-link">Card link</a>
+           <a href="#" class="card-link">Add to Cart</a>
            <a href="#" class="card-link">Another link</a>
        </div>
    </div>
+@endforeach
+
    </div>
 </div>
 @endsection
